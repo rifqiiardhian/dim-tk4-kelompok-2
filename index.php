cb7f8f068f8911af4761a20af679824d27116194
rifqiiardhian <rifqiardhian@gmail.com>
2023-02-03 21:30:13 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    // UNCOMMENT THE CODE BELOW (LN 4-19) IF YOU ARE USING XAMPP
+    // (e.g. full path (save the project on the htdocs) -> D:\xampp\htdocs\dim-tugas-kelompok-4)
+    $loc = "/dim-tugas-kelompok-4";
+
+    $request = $_SERVER['REQUEST_URI'];
+
+    switch ($request) {
+        case $loc.'/' :
+            require "views/home.php";
+            break;
+        case $loc.'/about' :
+            require "views/about.php";
+            break;
+        default:
+            http_response_code(404);
+            echo "404";
+            break;
+    }
+
+    // MACOS
+    // COMMENT THE CODE BELOW (LN 25-38) IF YOU ARE USING XAMPP
+    // $request = $_SERVER['REQUEST_URI'];
+
+    // switch ($request) {
+    //     case '/' :
+    //         require "views/home.php";
+    //         break;
+    //     case '/about' :
+    //         require "views/about.php";
+    //         break;
+    //     default:
+    //         http_response_code(404);
+    //         echo "404";
+    //         break;
+    // }
+?>
---
+++ views/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "views/components/meta.php" ?>
+
+    <title>Tugas Kelompok 4 | About</title>
+
+    <?php include "views/components/fonts.php" ?>
+
+    <link rel="stylesheet" href="/public/css/app.css" />
+</head>
+<body>
+    <?php include "views/components/navbar.php" ?>
+
+    <h1>About Page</h1>
+</body>
+</html>
